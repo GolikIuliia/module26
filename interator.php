48985743d4cddde7deeafa7707dffeed6e7275bf
GolikIuliia <116018335+GolikIuliia@users.noreply.github.com>
2023-06-22 21:37:21 +0300

    Update interator.php

--- interator.php
+++ interator.php
@@ -1,69 +1,63 @@
 <?php
-// interface interfaceIterator extends Traversable {
-//     /* Методы */
-//     public function rewind();
-//     public function newLine($str);
-//     public function next();
-//     public function valid();
-//     public function getTag(string $tag);
-//     public function getLines();
-//     }
-    
-class myIterator //implements interfaceIterator
+
+//var_dump($iter->getLines());
+
+
+
+class ExampleIter implements Iterator
 {
-    private $line;
-    private $ar = [];
 
-    public function __construct() 
-    { 
-        $this->line = 0;
-    }
+    protected $storage = [];
 
-    public function rewind(): void {
-        //var_dump(__METHOD__);
-        $this->line = 0;
-    }
-
-    public function newLine($str) {
-       // var_dump(__METHOD__);
-        return $this->ar[] = $str;
-    }
-
-    public function next(): void {
-       // var_dump(__METHOD__);
-        ++$this->line;
-    }
-
-    public function valid(): bool {
-        //var_dump(__METHOD__);
-        return isset($this->ar[$this->line]);
-    }
-
-    public function getTag(string $tag) 
-    { 
-        return (strpos($this->ar[$this->line], $tag) !== false);
-    }
-
-    public function getLines()
-    { 
-        return $this->ar;
-    }
-
-    public function deleteLine() 
-    { 
-        $this->ar[$this->line] = NULL;
-    }
-
-    public function current() 
+    public function set($key, $val)
     {
-        return $this->ar[$this->line];
+        $this->storage[$key] = $val;
+    }
+
+    public function get($key) 
+    {
+        return $this->storage[$key];
+    }
+
+    public function current() : mixed 
+    {
+        return current($this->storage);
+    }
+
+    public function key() : mixed
+    {
+        return key($this->storage);
+    }
+
+    public function next(): void
+    {
+        next($this->storage);
+    }
+
+    public function rewind(): void
+    {
+        reset($this->storage);
+    }
+
+    public function valid(): bool
+    {
+        return null !== key($this->storage);
+    }
+
+    public function remove() : void 
+    { 
+        unset($this->storage[$this->key()]);
     }
 }
-$iter = new myIterator;
+
+$iter = new ExampleIter;
 $fp = @fopen("int.txt", "r");
+
 if ($fp) {
+    $id = 0; 
     while (($buffer = fgets($fp, 4096)) !== false) {
-        $iter->newLine($buffer); 
+        $iter->set($id, $buffer);
+        ++$id;
     }
     if (!feof($fp)) {
         echo "Ошибка: fgets() неожиданно потерпел неудачу\n";
@@ -71,10 +65,24 @@ if ($fp) {
     fclose($fp);
 
 }
-foreach ($iter as $key => $value){
-    var_dump($key, $value);
-    echo "\n";
-}
-//var_dump($iter->getLines());
 
+foreach ($iter as $key => $value){
+    if(strpos($value, 'title') !== false)
+    { 
+        $iter->remove();
+    }
+    elseif(strpos($value, 'description') !== false)
+    {
+        $iter->remove();
+    }
+    elseif(strpos($value, 'keywords') !== false)
+    {
+        $iter->remove();
+    }
+    else 
+    { 
+        var_dump($key, $value);
+    }
+}
+ 
 ?>
